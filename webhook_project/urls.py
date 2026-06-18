a1129de91d63e1ce682843ff5dc38dfd698d9564
Abdullah Elsawy <abdallah666mo@gmail.com>
2026-06-18 13:08:38 +0300

    486486

--- webhook_project/urls.py
+++ webhook_project/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from .views import whatsapp_webhook, privacy_policy
 
 urlpatterns = [
-    path('webhook/', whatsapp_webhook),
+    path('webhook', whatsapp_webhook),
     path('privacy-policy/', privacy_policy),
 ]
--- webhook_project/views.py
+++ webhook_project/views.py
@@ -7,26 +7,12 @@ VERIFY_TOKEN = "nile_techno_verify"
 
 @csrf_exempt
 def whatsapp_webhook(request):
-    if request.method == "GET":
-        mode = request.GET.get("hub.mode")
-        token = request.GET.get("hub.verify_token")
-        challenge = request.GET.get("hub.challenge")
+    print("🔥🔥🔥 REQUEST HIT SERVER 🔥🔥🔥")
+    print("METHOD:", request.method)
+    print("PATH:", request.path)
+    print("BODY:", request.body.decode("utf-8"))
 
-        if mode == "subscribe" and token == VERIFY_TOKEN:
-            return HttpResponse(challenge)
-
-        return HttpResponse(status=403)
-
-    if request.method == "POST":
-        print("\n" + "=" * 100)
-        print("🔥 WEBHOOK CALLED")
-        print("PATH:", request.path)
-        print("METHOD:", request.method)
-        print("HEADERS:", dict(request.headers))
-        print("BODY:", request.body.decode("utf-8"))
-        print("=" * 100 + "\n")
-
-        return JsonResponse({"status": "ok"})
+    return JsonResponse({"status": "ok"})
 
 
 def privacy_policy(request):
